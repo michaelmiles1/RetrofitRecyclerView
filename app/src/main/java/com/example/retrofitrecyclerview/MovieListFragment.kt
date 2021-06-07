d8078cd8c38825c5ebea0394d6b46833f106b423
Michael Miles <michael.gage.miles@gmail.com>
2021-06-07 17:58:50 -0400

    - Added MovieListFragment and contained it in MainActivity - Added Data Binding to Gradle

---
+++ app/src/main/java/com/example/retrofitrecyclerview/MovieListFragment.kt
@@ -0,0 +1,17 @@
+package com.example.retrofitrecyclerview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MovieListFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding =
+    }
+}
