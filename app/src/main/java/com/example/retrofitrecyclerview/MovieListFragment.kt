faf5b6f9753a13c33628cc6ef916406c50ef6a69
Michael Miles <michael.gage.miles@gmail.com>
2021-06-08 10:39:01 -0400

    - Added RecyclerView and cleaned up Gradle - Binded view to recyclerView - Show MovieListFragment.kt inside MainActivity in XML

--- app/src/main/java/com/example/retrofitrecyclerview/MovieListFragment.kt
+++ app/src/main/java/com/example/retrofitrecyclerview/MovieListFragment.kt
@@ -5,13 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.retrofitrecyclerview.databinding.MovieListFragmentBinding
 
 class MovieListFragment: Fragment() {
+
+    private val viewModel: MovieViewModel by lazy {
+        ViewModelProvider(this).get(MovieViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding =
+        val binding = MovieListFragmentBinding.inflate(inflater)
+        //This allows binding to observe the LiveData
+        binding.lifecycleOwner = this
+
+        //This connects the xml variable with kotlin one
+        binding.viewModel = viewModel
+
+        //Connects an adapter to the xml recyclerView
+        binding.movieList.adapter =
+
+        //Return binding root as view
+        return binding.root
     }
 }
